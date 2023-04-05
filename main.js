c9a18d4df524232bd20bdae4b3bb26bc17d0aeb5
Dave89dc <127298096+Dave89dc@users.noreply.github.com>
2023-04-05 10:20:23 +0200

    Geometry

--- main.js
+++ main.js
@@ -22,84 +22,87 @@ console.log(triangle1.perimeter);
 console.log(triangle1.area);
 
 
-let parola = "This is an example!";
 
-function solution (str) {
+// ESERCIZI CODWARS
 
-    let newArray = [];
-    let strArray = str.split("");
-    for (let i = 0; i < strArray.length; i++) {
-        newArray.unshift(strArray[i]);
-    };
-    let newArrayToString = newArray.join("");
-    let newArray2 = newArrayToString.split(" ");
-    let newArray3 = [];
-    for (let i = 0; i < newArray2.length; i++) {
-        newArray3.unshift(newArray2[i]);
-    };
-    return newArray3.join(" ");
+// let parola = "This is an example!";
 
-};
+// function solution (str) {
 
-console.log(solution(parola));
+//     let newArray = [];
+//     let strArray = str.split("");
+//     for (let i = 0; i < strArray.length; i++) {
+//         newArray.unshift(strArray[i]);
+//     };
+//     let newArrayToString = newArray.join("");
+//     let newArray2 = newArrayToString.split(" ");
+//     let newArray3 = [];
+//     for (let i = 0; i < newArray2.length; i++) {
+//         newArray3.unshift(newArray2[i]);
+//     };
+//     return newArray3.join(" ");
+
+// };
+
+// console.log(solution(parola));
 
 
-let numbers = [5, 8, 12, 19, 22];
+// let numbers = [5, 8, 12, 19, 22];
 
-function sumOfThe2LowestPositiveNumbers(array) {
+// function sumOfThe2LowestPositiveNumbers(array) {
 
-    let newArray = [];
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] > 0) {
-            newArray.push(array[i]);
-        };
-    };
-    if(newArray.length >= 4) {
-        let lowNumber = newArray[0];
-        for (let i = 1; i < newArray.length; i++) {
-            if(lowNumber > newArray[i] && newArray[i].isInteger()) {
-                lowNumber = newArray[i];
-            };
-        };
-        console.log(lowNumber);
-        let lowNumber2 = newArray[1];
-        for (let i = 0; i < newArray.length; i++) {
-            if(lowNumber2 > newArray[i] && newArray[i] !== lowNumber && newArray[i].isInteger()) {
-                lowNumber2 = newArray[i];
-            };
-        };
-        console.log(lowNumber2)
-        sum = lowNumber + lowNumber2;
-        return sum;
-    };
-    
-};
-
-console.log(sumOfThe2LowestPositiveNumbers(numbers));
-
-// let sum = 0;
-//     let lowNumber = array[0];
-//     let lowNumber2 = array[1];
+//     let newArray = [];
+//     let sum = 0;
 //     for (let i = 0; i < array.length; i++) {
-//         if((array[i] > 0) >= 4) {
-//             for (let i = 1; i < array.length; i++) {
-//                 if(lowNumber > array[i]) {
-//                     lowNumber = array[i];
-//                     array.shift(array[i]);
-//                     newArray.push(lowNumber);
-//                 };
-//             };
-//             for (let i = 2; i < array.length; i++) {
-//                 if(lowNumber2 > array[i]) {
-//                     lowNumber2 = array[i];
-//                     array.shift(array[i]);
-//                     newArray.push(lowNumber2);
-//                 };
-//             };
+//         if(array[i] > 0) {
+//             newArray.push(array[i]);
 //         };
 //     };
-//     for (let i = 0; i < newArray.length; i++) {
-//         sum += newArray[i];
+//     if(newArray.length >= 4) {
+//         let lowNumber = newArray[0];
+//         for (let i = 1; i < newArray.length; i++) {
+//             if(lowNumber > newArray[i] && newArray[i].isInteger()) {
+//                 lowNumber = newArray[i];
+//             };
+//         };
+//         console.log(lowNumber);
+//         let lowNumber2 = newArray[1];
+//         for (let i = 0; i < newArray.length; i++) {
+//             if(lowNumber2 > newArray[i] && newArray[i] !== lowNumber && newArray[i].isInteger()) {
+//                 lowNumber2 = newArray[i];
+//             };
+//         };
+//         console.log(lowNumber2)
+//         sum = lowNumber + lowNumber2;
+//         return sum;
 //     };
-//     return sum;
+    
+// };
+
+// console.log(sumOfThe2LowestPositiveNumbers(numbers));
+
+// // let sum = 0;
+// //     let lowNumber = array[0];
+// //     let lowNumber2 = array[1];
+// //     for (let i = 0; i < array.length; i++) {
+// //         if((array[i] > 0) >= 4) {
+// //             for (let i = 1; i < array.length; i++) {
+// //                 if(lowNumber > array[i]) {
+// //                     lowNumber = array[i];
+// //                     array.shift(array[i]);
+// //                     newArray.push(lowNumber);
+// //                 };
+// //             };
+// //             for (let i = 2; i < array.length; i++) {
+// //                 if(lowNumber2 > array[i]) {
+// //                     lowNumber2 = array[i];
+// //                     array.shift(array[i]);
+// //                     newArray.push(lowNumber2);
+// //                 };
+// //             };
+// //         };
+// //     };
+// //     for (let i = 0; i < newArray.length; i++) {
+// //         sum += newArray[i];
+// //     };
+// //     return sum;
--- triangle.js
+++ triangle.js
@@ -4,6 +4,9 @@ class Triangle {
         this.pointA = pointA;
         this.pointB = pointB;
         this.pointC = pointC;
+        this.segment1 = new Segment(this.pointA, this.pointB);
+        this.segment2 = new Segment(this.pointB, this.pointC);
+        this.segment3 = new Segment(this.pointC, this.pointA);
     };
 
     static createNewTriangle(xA, yA, xB, yB, xC, yC) {
@@ -14,22 +17,16 @@ class Triangle {
     };
 
     get perimeter() {
-        const segment1 = new Segment(this.pointA, this.pointB);
-        const segment2 = new Segment(this.pointB, this.pointC);
-        const segment3 = new Segment(this.pointC, this.pointA);
-        const perimeter = segment1.length + segment2.length + segment3.length;
+        const perimeter = this.segment1.length + this.segment2.length + this.segment3.length;
         return perimeter;
     };
 
     get area() {
-        const segment1 = new Segment(this.pointA, this.pointB);
-        const segment2 = new Segment(this.pointB, this.pointC);
-        const segment3 = new Segment(this.pointC, this.pointA);
-        const perimeter = segment1.length + segment2.length + segment3.length;
-        const semiPerimeter = perimeter / 2;
-        const subSide1 = semiPerimeter - segment1.length;
-        const subSide2 = semiPerimeter - segment2.length;
-        const subSide3 = semiPerimeter - segment3.length;
+        // Formula di Erone:
+        const semiPerimeter = (this.perimeter) / 2;
+        const subSide1 = semiPerimeter - this.segment1.length;
+        const subSide2 = semiPerimeter - this.segment2.length;
+        const subSide3 = semiPerimeter - this.segment3.length;
         const area = Math.sqrt(semiPerimeter*subSide1*subSide2*subSide3);
         return area;
     };
